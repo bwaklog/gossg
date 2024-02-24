b80378fc5096b032fc678e573e8d4a2e21752ea2
bwaklog <aditya.mh@outlook.com>
2024-02-25 03:15:41 +0530

    something makes sense

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.4
 require (
 	github.com/yuin/goldmark v1.7.0
 	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1
 )
--- main.go
+++ main.go
@@ -4,17 +4,89 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
-	"github.com/bwaklog/ssg/src"
+	"gopkg.in/yaml.v3"
+	// "github.com/yuin/goldmark"
+	// "github.com/yuin/goldmark/extension"
+	// "github.com/yuin/goldmark/parser"
+	// "github.com/yuin/goldmark/renderer/html"
+	// "gopkg.in/yaml.v2"
 )
 
-func main() {
+type FrontMatters struct {
+	Title  string `yaml:"title"`
+	Author string `yaml:"author"`
+	Date   string `yaml:"date"`
+}
+
+type Post struct {
+	frontMatters FrontMatters
+	Body         string
+}
+
+// get posts from ./posts
+func getMdPosts() ([]Post, error) {
 	// get all posts from ./posts from os
-	files, err := os.ReadDir("./posts/")
+	files, err := os.ReadDir(postDir)
+
+	// fetch only md files from the list
+	for _, file := range files {
+		if file.IsDir() || filepath.Ext(file.Name()) != ".md" {
+			continue
+		}
+
+        inpMdFile := filepath.Join(postDir, file.Name())
+        outFile := filepath.Join(outputDir, file.Name())
+
+        fmt.Printf("inp : %q\tout: %q\n", inpMdFile, outFile)
+
+        mdFileContent, err := os.ReadFile(inpMdFile)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        postFrontMatter := parseFrontMatter(mdFileContent)
+
+        fmt.Printf("frontmatter: %q\n", postFrontMatter)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+
 	// list files in ./post
-	fmt.Printf("%q", files)
+
+	return []Post{}, nil
+}
+
+func generateHtmlFils() {}
+
+func parseFrontMatter(fileContentString []byte) FrontMatters {
+
+    frontmatter := &FrontMatters{}
+
+    if err := yaml.Unmarshal(fileContentString, frontmatter); err != nil {
+        log.Fatal(err)
+    }
+
+    // fmt.Printf("frontmatter: %q\n", frontmatter)
+    
+
+	return *frontmatter 
+}
+
+func serveSite() {}
+
+const (
+	postDir   = "posts"
+	outputDir = "output"
+)
+
+func main() {
+	_, err := getMdPosts()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
