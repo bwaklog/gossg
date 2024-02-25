d97c0b7ad7a454bdf206be6b1d55a3cf818a530d
bwaklog <aditya.mh@outlook.com>
2024-02-25 11:32:04 +0530

    unfinished

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/yuin/goldmark"
 	"gopkg.in/yaml.v3"
-	// "github.com/yuin/goldmark"
 	// "github.com/yuin/goldmark/extension"
 	// "github.com/yuin/goldmark/parser"
 	// "github.com/yuin/goldmark/renderer/html"
@@ -21,6 +24,7 @@ type FrontMatters struct {
 }
 
 type Post struct {
+	fileName     string
 	frontMatters FrontMatters
 	Body         string
 }
@@ -29,6 +33,7 @@ type Post struct {
 func getMdPosts() ([]Post, error) {
 	// get all posts from ./posts from os
 	files, err := os.ReadDir(postDir)
+	var posts []Post
 
 	// fetch only md files from the list
 	for _, file := range files {
@@ -36,57 +41,104 @@ func getMdPosts() ([]Post, error) {
 			continue
 		}
 
-        inpMdFile := filepath.Join(postDir, file.Name())
-        outFile := filepath.Join(outputDir, file.Name())
+		inpMdFile := filepath.Join(postDir, file.Name())
 
-        fmt.Printf("inp : %q\tout: %q\n", inpMdFile, outFile)
+		mdFileContent, err := os.ReadFile(inpMdFile)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        mdFileContent, err := os.ReadFile(inpMdFile)
-        if err != nil {
-            log.Fatal(err)
-        }
+		postFrontMatter := parseFrontMatter(mdFileContent)
+		fileContentString := strings.Split(string(mdFileContent), "---")[2]
+		parsedMarkdownContent := markdownParser([]byte(fileContentString))
 
-        postFrontMatter := parseFrontMatter(mdFileContent)
-
-        fmt.Printf("frontmatter: %q\n", postFrontMatter)
+		posts = append(posts, Post{
+			fileName:     file.Name(),
+			frontMatters: postFrontMatter,
+			Body:         parsedMarkdownContent.String(),
+		})
 	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
 	// list files in ./post
+	for _, post := range posts {
+		fmt.Printf("post: %q\n", post)
+	}
 
-	return []Post{}, nil
+	return posts, nil
 }
 
-func generateHtmlFils() {}
+func generateHTML(posts []Post) {
+
+
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		if err := os.Mkdir(outputDir, 0755); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for _, post := range posts {
+
+		postTemplate, err := template.ParseFiles("./template/post.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+        var buffer bytes.Buffer
+        err = postTemplate.ExecuteTemplate(&buffer, "post.html", post)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        outputFile := strings.Join([]string{outputDir, strings.TrimSuffix(post.fileName, ".md") + ".html"}, "/")
+
+
+        if err := os.WriteFile(outputFile, buffer.Bytes(), 0644); err != nil {
+            log.Fatal(err)
+        }
+
+	}
+}
 
 func parseFrontMatter(fileContentString []byte) FrontMatters {
 
-    frontmatter := &FrontMatters{}
+	frontmatter := &FrontMatters{}
 
-    if err := yaml.Unmarshal(fileContentString, frontmatter); err != nil {
-        log.Fatal(err)
-    }
+	if err := yaml.Unmarshal(fileContentString, frontmatter); err != nil {
+		log.Fatal(err)
+	}
 
-    // fmt.Printf("frontmatter: %q\n", frontmatter)
-    
+	return *frontmatter
+}
 
-	return *frontmatter 
+func markdownParser(markdownContent []byte) bytes.Buffer {
+	var buffer bytes.Buffer
+	if err := goldmark.Convert(markdownContent, &buffer); err != nil {
+		log.Fatal(err)
+	}
+	return buffer
 }
 
 func serveSite() {}
 
 const (
-	postDir   = "posts"
-	outputDir = "output"
+	postDir      = "posts"
+	outputDir    = "rendered"
+	templatePath = "./template/"
 )
 
 func main() {
-	_, err := getMdPosts()
+	posts, err := getMdPosts()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	generateHTML(posts)
+
+	///for _, post := range posts {
+	//	fmt.Printf("post: %q\n", post)
+	//}
 }
